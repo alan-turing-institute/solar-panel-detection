1e97f4a5d67556369faef685f74e548abf598f70
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-01-16 10:45:56 +0000

    REPD csv cleaning automated

--- database/pre_process.py
+++ database/pre_process.py
@@ -1,21 +1,25 @@
 import pandas as pd
+import re
 
-repd = "../data/raw/repd_modified.csv"
-# fit = "../data/raw/feed-in_tariff_installation_report_30_september_2019.csv"
-# osm = "../data/raw/osm_compile_processed_PV_objects_modified.csv"
+def clean_repd_csv(csv_str):
+    csv_str = csv_str.replace("\r", "").replace("†", "")
+    rgx_list = ['\s*<br*\s*"', '\s*<br>\s*"']
+    for rgx_match in rgx_list:
+        csv_str = re.sub(rgx_match, '"', csv_str)
+    return csv_str
 
-repd_df = pd.read_csv(repd, encoding = "ISO-8859-1")
-# fit_df = pd.read_csv(fit, encoding = "ISO-8859-1")
-# osm_df = pd.read_csv(osm, encoding = "ISO-8859-1")
+# File paths
+repd_file_path = "../data/data/as_received/REPD/Public-Database-September-2019.csv"
+modified_repd_file_path = "../data/data/raw/repd_modified_processed.csv"
 
+# Make edits to specific columns with pandas
+repd_df = pd.read_csv(repd_file_path, encoding = "ISO-8859-1", skiprows=1)
 repd_df['Storage Co-location REPD Ref ID'] = repd_df['Storage Co-location REPD Ref ID'].map(lambda x: float(str(x).replace(',','')))
 repd_df['FiT Tariff (p/kWh)'] = repd_df['FiT Tariff (p/kWh)'].map(lambda x: float(x))
 repd_df.Address = repd_df.Address.str.replace('\r\n', ', ')
 repd_df['Appeal Reference'] = repd_df['Appeal Reference'].map(lambda x: str(x).replace('\r\n', ''))
-
-modified_repd_file_path = "../data/raw/repd_modified_processed.csv"
 repd_csv_str = repd_df.to_csv(index=False)
 
+# Make generic edits and save modified csv
 with open(modified_repd_file_path, 'w') as repdfile:
-    temp = repd_csv_str.replace("\r", "")
-    repdfile.write(temp)
+    repdfile.write(clean_repd_csv(repd_csv_str))
