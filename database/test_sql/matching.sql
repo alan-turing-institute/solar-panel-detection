77fa6f509c70cd8539501ab38f36414692b36cbd
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-01-22 11:09:12 +0000

    update repd_id filter code to search for osm entries within 200m of repd

--- database/test_sql/matching.sql
+++ database/test_sql/matching.sql
@@ -1,7 +1,7 @@
--- I think this shows the nearest osm entry to the repd entry with repd_id 1285
+-- This shows the osm entries close to repd entries, but only those with an repd_id already
 -- Using nearest neighbour search
-select solar.osm.osm_id, solar.osm_repd_id_mapping.repd_id, solar.repd.repd_id
+select solar.osm.osm_id, solar.osm_repd_id_mapping.repd_id, solar.repd.repd_id as matching_repd_id
 from solar.osm, solar.repd, solar.osm_repd_id_mapping
-where solar.osm_repd_id_mapping.repd_id = 1285 and solar.osm.osm_id = solar.osm_repd_id_mapping.osm_id
-order by ST_Distance(solar.osm.geom, solar.repd.geom)
-ASC LIMIT 1;
+where solar.osm.osm_id = solar.osm_repd_id_mapping.osm_id
+and solar.osm.geom && ST_Expand(solar.repd.geom, 200) -- Find all those within 200m
+order by ST_Distance(solar.osm.geom, solar.repd.geom);
