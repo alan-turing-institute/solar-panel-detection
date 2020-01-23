4ae9e446f6f484bc464a570c0acedac37ca6c086
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-01-23 14:29:50 +0000

    find closest repd to osm with repd id

--- database/test_sql/matching.sql
+++ database/test_sql/matching.sql
@@ -1,5 +1,19 @@
 -- This shows the osm entries close to repd entries, but only those with an repd_id already
 -- Using nearest neighbour search
+select solar.osm.osm_id, solar.osm_repd_id_mapping.repd_id, solar.repd.repd_id as closest_repd_id, solar.repd.co_location_repd_id
+from solar.osm, solar.repd, solar.osm_repd_id_mapping
+where solar.osm.osm_id = solar.osm_repd_id_mapping.osm_id
+order by ST_Distance(solar.osm.geom, solar.repd.geom);
+
+-- Same as above but excluding the matching repd id
+select solar.osm.osm_id, solar.osm_repd_id_mapping.repd_id, solar.repd.repd_id as closest_repd_id, solar.repd.co_location_repd_id
+from solar.osm, solar.repd, solar.osm_repd_id_mapping
+where solar.osm.osm_id = solar.osm_repd_id_mapping.osm_id
+and solar.osm_repd_id_mapping.repd_id != solar.repd.repd_id
+order by ST_Distance(solar.osm.geom, solar.repd.geom);
+
+-- This shows the osm entries close to repd entries, but only those with an repd_id already
+-- Using nearest neighbour search
 select solar.osm.osm_id, solar.osm_repd_id_mapping.repd_id, solar.repd.repd_id as matching_repd_id
 from solar.osm, solar.repd, solar.osm_repd_id_mapping
 where solar.osm.osm_id = solar.osm_repd_id_mapping.osm_id
