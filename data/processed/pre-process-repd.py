6c1063293fe552b08f078fe80edb27dc058c4f65
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-01-23 13:52:39 +0000

    add repd lat and lon to alternate python script

--- data/processed/pre-process-repd.py
+++ data/processed/pre-process-repd.py
@@ -2,6 +2,7 @@
 ### Process the REPD data to remove oddities
 ### Reads from stdin, writes to stdout
 
+from bng_to_latlon import OSGB36toWGS84 as convert
 import sys
 import pandas as pd
 import re
@@ -25,14 +26,22 @@ repd_df['Y-coordinate'] = repd_df['Y-coordinate'].map(lambda x: float(str(x).rep
 # Remove spaces from postcodes
 repd_df['Post Code'] = repd_df['Post Code'].map(lambda x: str(x).replace(' ',''))
 
-# Ensure the tariff is numeric 
+# Ensure the tariff is numeric
 repd_df['FiT Tariff (p/kWh)'] = repd_df['FiT Tariff (p/kWh)'].map(lambda x: float(x))
 
 # Remove line breaks from within certain fields
 repd_df['Address'] = repd_df['Address'].str.replace('\r\n', ', ')
 repd_df['Appeal Reference'] = repd_df['Appeal Reference'].map(lambda x: str(x).replace('\r\n', ''))
 
+# Convert each BNG coordinate to lat and lon and add new columns
+for index, row in repd_df.iterrows():
+    # Ignore any rows that don't have an X and Y coordinate
+    if pd.notna(row['X-coordinate']) and pd.notna(row['Y-coordinate']):
+        lat, lon = convert(row['X-coordinate'], row['Y-coordinate'])
+        repd_df.set_value(index,'latitude', lat)
+        repd_df.set_value(index,'longitude', lon)
+
 repd_csv_str = repd_df.to_csv(index=False)
 
-# Make generic edits and write out 
+# Make generic edits and write out
 sys.stdout.write(clean_repd_csv(repd_csv_str))
