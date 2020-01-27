132a5aa50381f3fbe233f723d04f2317b4bff113
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-01-27 15:51:34 +0000

    remove attempted duplicate deletion

--- database/osm-repd-all-distances.sql
+++ database/osm-repd-all-distances.sql
@@ -12,12 +12,3 @@ into osm_repd_all_distances
 from solar.osm, solar.repd, solar.osm_repd_id_mapping
 where solar.osm.osm_id = solar.osm_repd_id_mapping.osm_id
 order by ST_Distance(solar.osm.geom::geography, solar.repd.geom::geography);
-
--- Remove duplicates so we only have the closest match for each osm entry
-delete
-from
-    osm_repd_all_distances a
-        using osm_repd_all_distances b
-where
-    a.distance_meters < b.distance_meters
-    and a.closest_geo_match_from_repd_repd_id = b.closest_geo_match_from_repd_repd_id;
