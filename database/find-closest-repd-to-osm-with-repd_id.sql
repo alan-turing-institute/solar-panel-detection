2a6452efa73ca84fc51cc5049e15c1d591862f71
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-01-27 15:50:36 +0000

    find closest repd osm with repd id

---
+++ database/find-closest-repd-to-osm-with-repd_id.sql
@@ -0,0 +1,15 @@
+select
+  solar.osm.osm_id,
+  solar.osm_repd_id_mapping.repd_id as repd_id_in_osm,
+  closest_pt.repd_id as closest_geo_match_from_repd_repd_id,
+  closest_pt.distance_meters
+into osm_repd_closest
+from solar.osm, solar.osm_repd_id_mapping
+CROSS JOIN LATERAL
+  (SELECT
+     solar.repd.repd_id,
+     solar.osm.geom::geography <-> solar.repd.geom::geography as distance_meters
+     FROM solar.repd
+     ORDER BY solar.osm.geom::geography <-> solar.repd.geom::geography
+   LIMIT 1) AS closest_pt
+where solar.osm.osm_id = solar.osm_repd_id_mapping.osm_id;
