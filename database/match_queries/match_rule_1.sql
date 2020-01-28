71b3edac9b5b9e7aea7d75d6ac4da3d3340645f8
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-01-28 15:16:22 +0000

    use table to store match results

--- database/match_queries/match_rule_1.sql
+++ database/match_queries/match_rule_1.sql
@@ -2,10 +2,15 @@
 -- 1. Create DB with database/solar_db.sql
 -- 2. Create osm_repd_closest table with database/osm-repd-location-tables/find-closest-repd-to-osm-inc-no-repd_id.sql
 
+drop table if exists match_rule_1_results;
 select
   osm_repd_closest.osm_id,
+  solar.osm_repd_id_mapping.repd_id as osm_repd_id,
   osm_repd_closest.closest_geo_match_from_repd_repd_id,
-  solar.osm_repd_id_mapping.repd_id as osm_repd_id
+  osm_repd_closest.distance_meters
+into match_rule_1_results
 from osm_repd_closest
 left join solar.osm_repd_id_mapping on osm_repd_closest.osm_id = solar.osm_repd_id_mapping.osm_id
 where osm_repd_closest.distance_meters < 250;
+
+select * from match_rule_1_results;
