3b9c12e31e4a2f63aca934b8ead3ae75690bfa29
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-02-07 11:39:40 +0000

    update deduplication table to avoid nodes

--- db/round_two/osm-deduplication.sql
+++ db/round_two/osm-deduplication.sql
@@ -1,11 +1,11 @@
-drop table if exists osm_neighbours;
+-- drop table if exists osm_neighbours;
 drop table if exists temp;
 
 -- the purpose of the below is to find objects that are oddly close and check if they are part of the same thing
 select *
 into temp
 from osm;
--- todo: Perhaps we should modify this to be only for relations/ways if takes too long with the nodes
+
 select
   osm.osm_id,
   closest_pt.osm_id as neighbour_osm_id,
@@ -17,5 +17,20 @@ CROSS JOIN LATERAL
      temp.osm_id,
      osm.location::geography <-> temp.location::geography as distance_meters
      FROM temp
+     where temp.objtype != 'node'
      ORDER BY osm.location::geography <-> temp.location::geography
-   LIMIT 1) AS closest_pt;
+   LIMIT 2) AS closest_pt
+where osm.objtype != 'node'
+and osm.osm_id != closest_pt.osm_id;
+
+drop table temp;
+
+select
+  osm_neighbours.osm_id,
+  osm_neighbours.neighbour_osm_id,
+  osm_neighbours.distance_meters,
+  osm.latitude,
+  osm.longitude
+from osm_neighbours, osm
+where osm_neighbours.osm_id = osm.osm_id
+order by osm_neighbours.distance_meters asc;
