62d16fd33bff24bbbaf4eaf4611a74d35db0dfe8
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-03-09 17:14:56 +0000

    add distance metrics for REPD grouping

--- explorations/round_two/repd-deduplication/find-duplicates.sql
+++ explorations/round_two/repd-deduplication/find-duplicates.sql
@@ -1,5 +1,7 @@
 drop table if exists repd_all_distances;
 drop table if exists temp;
+CREATE EXTENSION if not exists fuzzystrmatch;
+CREATE EXTENSION if not exists pg_trgm;
 -- Duplicate this table so we can compare it against itself:
 select *
 into temp
@@ -10,16 +12,18 @@ where operational is not null;
 -- within a certain distance (e.g. 300m)
 select
   repd.site_name,
-  repd.repd_id,
-  repd.co_location_repd_id as co_id,
   closest_pt.site_name as neighbour_name,
+  levenshtein(repd.site_name, closest_pt.site_name, 0,2,2) as ld, -- not penalising insertions makes sense only if we can guarantee the 2nd arg is longer ( we can't)
+  similarity(repd.site_name, closest_pt.site_name) as sd,
+  repd.repd_id,
+  -- repd.co_location_repd_id as co_id,
   closest_pt.repd_id as neighbour_id,
-  closest_pt.co_location_repd_id as neighbour_co_id,
-  closest_pt.distance_meters,
-  repd.latitude, -- Having these enables manual checking
-  repd.longitude,
-  closest_pt.latitude as neighbour_lat, -- Having these enables manual checking
-  closest_pt.longitude as neighbour_lon
+  -- closest_pt.co_location_repd_id as neighbour_co_id,
+  closest_pt.distance_meters
+  -- repd.latitude, -- Having these enables manual checking
+  -- repd.longitude,
+  -- closest_pt.latitude as neighbour_lat, -- Having these enables manual checking
+  -- closest_pt.longitude as neighbour_lon
 into repd_all_distances
 from repd
 CROSS JOIN LATERAL
@@ -33,10 +37,14 @@ CROSS JOIN LATERAL
      FROM temp
      ORDER BY repd.location::geography <-> temp.location::geography) AS closest_pt
 where repd.repd_id != closest_pt.repd_id
-and repd.operational is not null
+-- and repd.operational is not null
 and closest_pt.distance_meters < 2000
 order by closest_pt.distance_meters asc;
 
 drop table temp;
 
-select * from repd_all_distances;
+select
+  *
+from repd_all_distances;
+-- where sd < 0.5
+-- where site_name % ANY(STRING_TO_ARRAY(neighbour_name,' '));/
