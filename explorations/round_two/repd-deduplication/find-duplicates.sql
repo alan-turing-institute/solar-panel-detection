26835acbf9f6ad6e606b1c73aa56b8ce927d917c
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-03-10 11:12:19 +0000

    find possible and definite duplicates REPD

--- explorations/round_two/repd-deduplication/find-duplicates.sql
+++ explorations/round_two/repd-deduplication/find-duplicates.sql
@@ -12,14 +12,16 @@ where operational is not null;
 -- within a certain distance (e.g. 300m)
 select
   repd.site_name,
-  closest_pt.site_name as neighbour_name,
-  levenshtein(repd.site_name, closest_pt.site_name, 0,2,2) as ld, -- not penalising insertions makes sense only if we can guarantee the 2nd arg is longer ( we can't)
-  similarity(repd.site_name, closest_pt.site_name) as sd,
+  replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(repd.site_name, 'solar', ''), 'Solar', ''), 'park', ''), 'Park', ''), 'farm', ''), 'Farm', ''), '(resubmission)', ''), '(Resubmission)', ''), 'extension', ''), 'resubmission', ''), ' - ', ''), 'Extension', ''), '  ', ' '), '()', '') as site_name_reduced,
+  closest_pt.site_name as neighbour_site_name,
+  replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(replace(closest_pt.site_name, 'solar', ''), 'Solar', ''), 'park', ''), 'Park', ''), 'farm', ''), 'Farm', ''), '(resubmission)', ''), '(Resubmission)', ''), 'extension', ''), 'resubmission', ''), ' - ', ''), 'Extension', ''), '  ', ' '), '()', '') as neighbour_site_name_reduced,
   repd.repd_id,
   -- repd.co_location_repd_id as co_id,
   closest_pt.repd_id as neighbour_id,
   -- closest_pt.co_location_repd_id as neighbour_co_id,
-  closest_pt.distance_meters
+  closest_pt.distance_meters,
+  repd.capacity,
+  closest_pt.capacity as neighbour_cap
   -- repd.latitude, -- Having these enables manual checking
   -- repd.longitude,
   -- closest_pt.latitude as neighbour_lat, -- Having these enables manual checking
@@ -33,18 +35,49 @@ CROSS JOIN LATERAL
      temp.co_location_repd_id,
      repd.location::geography <-> temp.location::geography as distance_meters,
      temp.latitude, -- Having these enables manual checking
-     temp.longitude
+     temp.longitude,
+     temp.capacity
      FROM temp
      ORDER BY repd.location::geography <-> temp.location::geography) AS closest_pt
 where repd.repd_id != closest_pt.repd_id
 -- and repd.operational is not null
-and closest_pt.distance_meters < 2000
+and closest_pt.distance_meters < 1380
 order by closest_pt.distance_meters asc;
 
 drop table temp;
 
-select
-  *
+drop table if exists repd_proximity_matches;
+select -- This should NOT select definite matches
+  site_name,
+  neighbour_site_name,
+  site_name_reduced,
+  neighbour_site_name_reduced,
+  -- levenshtein(site_name_reduced, neighbour_site_name_reduced, 1, 1, 2) as ld, -- not penalising insertions makes sense only if we can guarantee the 2nd arg is longer ( we can't)
+  similarity(site_name_reduced, neighbour_site_name_reduced) as sd,
+  repd_id,
+  neighbour_id,
+  distance_meters,
+  capacity,
+  neighbour_cap
+into repd_proximity_matches
 from repd_all_distances;
--- where sd < 0.5
--- where site_name % ANY(STRING_TO_ARRAY(neighbour_name,' '));/
+
+drop table if exists possible_repd_duplicates;
+select *
+into possible_repd_duplicates
+from repd_proximity_matches
+where sd < 0.2
+and distance_meters > 0;
+order by distance_meters asc;
+
+drop table if exists repd_duplicates;
+select *
+into repd_duplicates
+from repd_proximity_matches
+where sd >= 0.2
+or distance_meters = 0;
+order by distance_meters asc;
+
+select count(*) from repd_proximity_matches;
+select count(*) from possible_repd_duplicates;
+select count(*) from repd_duplicates;
