727cefb67b4f1c7ce2b620c8a106a563b052467a
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-03-16 16:07:49 +0000

    create osm_with_repd_id_repd_closest from other table

--- explorations/round_two/osm-repd/find-closest-repd-to-osm-with-repd_id.sql
+++ explorations/round_two/osm-repd/find-closest-repd-to-osm-with-repd_id.sql
@@ -1,21 +1,14 @@
 drop table if exists osm_with_repd_id_repd_closest;
 select
-  osm.osm_id,
+  osm_repd_closest.osm_id,
   osm_repd_id_mapping.repd_id as repd_id_in_osm,
-  closest_pt.repd_id as closest_geo_match_from_repd_repd_id,
-  closest_pt.co_location_repd_id as closest_geo_match_from_repd_co_location_repd_id,
-  closest_pt.distance_meters
+  osm_repd_closest.closest_geo_match_from_repd_repd_id,
+  osm_repd_closest.closest_geo_match_from_repd_co_location_repd_id,
+  osm_repd_closest.distance_meters,
+  osm_repd_closest.site_name
 into osm_with_repd_id_repd_closest
-from osm, osm_repd_id_mapping
-CROSS JOIN LATERAL
-  (SELECT
-     repd.repd_id,
-     repd.co_location_repd_id,
-     osm.location::geography <-> repd.location::geography as distance_meters
-     FROM repd
-     ORDER BY osm.location::geography <-> repd.location::geography
-   LIMIT 1) AS closest_pt
-where osm.osm_id = osm_repd_id_mapping.osm_id;
+from osm_repd_closest, osm_repd_id_mapping
+where osm_repd_closest.osm_id = osm_repd_id_mapping.osm_id;
 
 -- OSM with REPD id with closest geographical match in REPD having that repd_id and co-located REPD id:
 -- See Match Rule 11
