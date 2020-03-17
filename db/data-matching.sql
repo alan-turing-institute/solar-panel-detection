9a96a983d9fc593a9fe1bff51d9e13b570a37fe8
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-03-17 16:16:18 +0000

    update match rule 1

--- db/data-matching.sql
+++ db/data-matching.sql
@@ -39,11 +39,19 @@ select osm_repd_neighbours.osm_id,
   where osm_repd_neighbours.osm_id = osm_repd_id_mapping.osm_id;
 
 -- Match rule 1 (see doc/matching.md)
+drop table if exists repd_copy;
+select * into repd_copy from repd;
 insert into matches
 select repd.master_repd_id, osm.master_osm_id
-  from osm_with_existing_repd_neighbours, osm, repd
+  from osm_with_existing_repd_neighbours, osm, repd, repd_copy
+  -- Table linking:
   where osm_with_existing_repd_neighbours.osm_id = osm.osm_id
   and osm_with_existing_repd_neighbours.closest_geo_match_from_repd_repd_id = repd.repd_id
-  and (osm_with_existing_repd_neighbours.repd_id_in_osm = osm_with_existing_repd_neighbours.closest_geo_match_from_repd_repd_id
+  and osm_with_existing_repd_neighbours.repd_id_in_osm = repd_copy.repd_id
+  -- Matching relevant:
+  and   (osm_with_existing_repd_neighbours.repd_id_in_osm = osm_with_existing_repd_neighbours.closest_geo_match_from_repd_repd_id
       or osm_with_existing_repd_neighbours.repd_id_in_osm = osm_with_existing_repd_neighbours.closest_geo_match_from_repd_co_location_repd_id
-      or osm_with_existing_repd_neighbours.repd_id_in_osm = repd.master_repd_id);
+      or osm_with_existing_repd_neighbours.repd_id_in_osm = repd.master_repd_id
+      or osm_with_existing_repd_neighbours.closest_geo_match_from_repd_repd_id = repd_copy.co_location_repd_id
+      or osm_with_existing_repd_neighbours.closest_geo_match_from_repd_repd_id = repd_copy.master_repd_id
+    );
--- doc/matching.md
+++ doc/matching.md
@@ -7,4 +7,10 @@ This note documents the matching of entities between the various datasets in the
 # Match Rules:
 
 1. The nearest neighbour in meters of OSM entries to REPD entries where the REPD ID is the same as that already present in OSM data. Accept those that are the same (regardless of distance).
-2. Nearest neighbour for remaining OSM where the `objtype` is `way` or `relation` and the object is a "group master" (the `osm_id = master_osm_id` or `repd_id = master_repd_id`). Accept all matches that are below a distance threshold and don't exceed a capacity difference of X.
+2. Nearest neighbour for remaining OSM/REPD where the OSM `objtype` is `way` or `relation` and the object is a "group master" (the `osm_id = master_osm_id` or `repd_id = master_repd_id`). Accept all matches that are below a distance threshold and don't exceed a capacity difference of X.
+
+# Matches
+
+| Rule | Count |
+| ---  | ---   |
+|  1   |  880  |
