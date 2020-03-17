7e79fd85b9a748080fec9cf37068dbce0279d272
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-03-17 17:14:25 +0000

    add match rule 0 into matching code

--- db/data-matching.sql
+++ db/data-matching.sql
@@ -3,6 +3,7 @@
 
 drop table if exists matches;
 create table matches (
+  match_rule     integer,
   master_repd_id integer,
   master_osm_id  bigint,
   mv_id          integer,
@@ -38,16 +39,34 @@ select osm_repd_neighbours.osm_id,
   from osm_repd_neighbours, osm_repd_id_mapping
   where osm_repd_neighbours.osm_id = osm_repd_id_mapping.osm_id;
 
--- Match rule 1 (see doc/matching.md)
+-- Match rule 0 (see doc/matching.md)
 drop table if exists repd_copy;
 select * into repd_copy from repd;
 insert into matches
-select repd.master_repd_id, osm.master_osm_id
+select 0, repd_copy.master_repd_id, osm.master_osm_id
   from osm_with_existing_repd_neighbours, osm, repd, repd_copy
   -- Table linking:
   where osm_with_existing_repd_neighbours.osm_id = osm.osm_id
   and osm_with_existing_repd_neighbours.closest_geo_match_from_repd_repd_id = repd.repd_id
   and osm_with_existing_repd_neighbours.repd_id_in_osm = repd_copy.repd_id
+  -- Matching relevant:
+  and repd.location::geography <-> repd_copy.location::geography < 500;
+
+-- Match rule 1 (see doc/matching.md)
+insert into matches
+select 1, repd.master_repd_id, osm.master_osm_id
+  from osm_with_existing_repd_neighbours, osm, repd, repd_copy
+  -- Table linking:
+  where osm_with_existing_repd_neighbours.osm_id = osm.osm_id
+  and osm_with_existing_repd_neighbours.closest_geo_match_from_repd_repd_id = repd.repd_id
+  and osm_with_existing_repd_neighbours.repd_id_in_osm = repd_copy.repd_id
+  -- Not matches already found:
+  and not exists (
+    select
+    from matches
+    where matches.master_repd_id = repd.master_repd_id
+    or matches.master_osm_id = osm.master_osm_id
+  )
   -- Matching relevant:
   and   (osm_with_existing_repd_neighbours.repd_id_in_osm = osm_with_existing_repd_neighbours.closest_geo_match_from_repd_repd_id
       or osm_with_existing_repd_neighbours.repd_id_in_osm = osm_with_existing_repd_neighbours.closest_geo_match_from_repd_co_location_repd_id
