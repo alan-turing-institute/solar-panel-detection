8eedbf15784edf8ce04b744fa6de0b6e62fd7b4d
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-03-18 14:40:35 +0000

    update relevance order

--- db/data-matching-rules/rule-3.sql
+++ db/data-matching-rules/rule-3.sql
@@ -7,7 +7,6 @@ select 3, repd.master_repd_id, osm.master_osm_id
   -- Table linking:
   where osm_repd_neighbours.osm_id = osm.osm_id
   and osm_repd_neighbours.closest_geo_match_from_repd_repd_id = repd.repd_id
-  and osm.objtype != 'node'
   and osm.osm_id = osm.master_osm_id
   and repd.repd_id = repd.master_repd_id
   -- Not matches already found:
@@ -18,4 +17,5 @@ select 3, repd.master_repd_id, osm.master_osm_id
     or matches.master_osm_id = osm.master_osm_id
   )
   -- Matching relevant:
-  and osm_repd_neighbours.distance_meters < 700;
+  and osm_repd_neighbours.distance_meters < 700
+  and osm.objtype != 'node';
