c1acc4316569cd5791f09bc62414adfeb7ce7a88
Ed Chalstrey <echalstrey@MAC-ATI0379.corp.turing.ac.uk>
2020-03-18 09:53:50 +0000

    match rule 2 distance threshold set

--- explorations/final_round/match-rule-2.sql
+++ explorations/final_round/match-rule-2.sql
@@ -1,27 +1,19 @@
 -- Match rule 2 (see doc/matching.md)
 -- insert into matches
 
-select *
-into repd_copy
-from repd;
-
--- TODO: modify so that we don't only get those with an repd in osm
-drop table if exists temp_match;
 select repd.master_repd_id,
        osm.master_osm_id,
-       osm_with_existing_repd_neighbours.distance_meters,
+       osm_repd_neighbours.distance_meters,
        CONCAT  (osm.objtype, '/', osm.osm_id) AS "osm",
        repd.latitude as r_lat,
        repd.longitude as r_lon,
        repd.site_name,
-       repd_copy.repd_id,
-       repd_copy.site_name
+       osm.capacity as osm_cap,
+       repd.capacity as repd_cap
 
-  from osm_with_existing_repd_neighbours, osm, repd, osm_repd_id_mapping, repd_copy
-  where osm_with_existing_repd_neighbours.osm_id = osm.osm_id
-  and osm_repd_id_mapping.osm_id = osm_with_existing_repd_neighbours.osm_id
-  and repd_copy.repd_id = osm_repd_id_mapping.repd_id
-  and osm_with_existing_repd_neighbours.closest_geo_match_from_repd_repd_id = repd.repd_id
+  from osm_repd_neighbours, osm, repd
+  where osm_repd_neighbours.osm_id = osm.osm_id
+  and osm_repd_neighbours.closest_geo_match_from_repd_repd_id = repd.repd_id
   and osm.objtype != 'node'
   and osm.osm_id = osm.master_osm_id
   and repd.repd_id = repd.master_repd_id
@@ -31,4 +23,5 @@ select repd.master_repd_id,
     where matches.master_repd_id = repd.master_repd_id
     or matches.master_osm_id = osm.master_osm_id
   )
-  order by osm_with_existing_repd_neighbours.distance_meters;
+  and osm_repd_neighbours.distance_meters < 700
+  order by osm_repd_neighbours.distance_meters;
